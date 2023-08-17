eb52e22f68d8a1df53521b03ec7151e8f66709b4
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-08-16 23:08:31 -0300

    feat: generating the directories and files
    need to actually populate the files now

--- src/main.s
+++ src/main.s
@@ -1,12 +1,16 @@
 .section .data
   proj_name: .asciz "/teste"
-  proj_src: .asciz "/src"
   proj_dir: .space 256
+  proj_src: .asciz "/src"
   proj_src_dir: .space 256
+  proj_cmake: .asciz "/CMakeLists.txt"
+  proj_cmake_dir: .space 256
+  proj_cmake_src_dir: .space 256
+  proj_main_file: .asciz "/main.cpp"
+  proj_main_file_dir: .space 256
 
 .section .bss
-cwd:
-  .skip 256
+cwd: .space 256
 
 .section .text
 .globl _start
@@ -18,7 +22,7 @@ _start:
   movq $256, %rsi
   syscall
 
-.make_proj_dir:
+.make_proj_path:
   movq $cwd, %rsi
   movq $proj_dir, %rdi
   call copy_string
@@ -27,7 +31,7 @@ _start:
   movq $proj_dir, %rdi
   call append_string
 
-.make_proj_src_dir:
+.make_proj_src_path:
   movq $proj_dir, %rsi
   movq $proj_src_dir, %rdi
   call copy_string
@@ -39,15 +43,56 @@ _start:
 .make_dirs:
   movq $83, %rax
   movq $proj_dir, %rdi
-  movq $0x555, %rsi
+  movq $0777, %rsi
   syscall
 
   movq $83, %rax
   movq $proj_src_dir, %rdi
-  movq $0x555, %rsi
+  movq $0777, %rsi
   syscall
 
-.print_string:
+.make_file_paths:
+  movq $proj_dir, %rsi
+  movq $proj_cmake_dir, %rdi
+  call copy_string
+
+  movq $proj_cmake, %rsi
+  movq $proj_cmake_dir, %rdi
+  call append_string
+
+  movq $proj_src_dir, %rsi
+  movq $proj_cmake_src_dir, %rdi
+  call copy_string
+
+  movq $proj_cmake, %rsi
+  movq $proj_cmake_src_dir, %rdi
+  call append_string
+
+  movq $proj_src_dir, %rsi
+  movq $proj_main_file_dir, %rdi
+  call copy_string
+
+  movq $proj_main_file, %rsi
+  movq $proj_main_file_dir, %rdi
+  call append_string
+
+.make_files:
+  movq $85, %rax
+  movq $proj_cmake_dir, %rdi
+  movq $0222, %rsi
+  syscall
+
+  movq $85, %rax
+  movq $proj_cmake_src_dir, %rdi
+  movq $0222, %rsi
+  syscall
+
+  movq $85, %rax
+  movq $proj_main_file_dir, %rdi
+  movq $0222, %rsi
+  syscall
+
+.print_strings:
   movq $1, %rax
   movq $1, %rdi
   movq $proj_src_dir, %rsi
