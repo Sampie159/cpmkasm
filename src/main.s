f3f6cfacb3f37dfbd739ade23d1d27ba67299876
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-08-17 00:07:12 -0300

    feat: successfully creates the project for C++
    now need to expand functionality to accept user input and to generate C projects

--- src/main.s
+++ src/main.s
@@ -1,16 +1,44 @@
 .section .data
-  proj_name: .asciz "/teste"
-  proj_dir: .space 256
-  proj_src: .asciz "/src"
-  proj_src_dir: .space 256
-  proj_cmake: .asciz "/CMakeLists.txt"
-  proj_cmake_dir: .space 256
-  proj_cmake_src_dir: .space 256
-  proj_main_file: .asciz "/main.cpp"
-  proj_main_file_dir: .space 256
+proj_name: .asciz "/teste"
+proj_dir: .space 256
+proj_src: .asciz "/src"
+proj_src_dir: .space 256
+proj_cmake: .asciz "/CMakeLists.txt"
+proj_cmake_dir: .space 256
+proj_cmake_src_dir: .space 256
+proj_main_file: .asciz "/main.cpp"
+proj_main_file_dir: .space 256
+cmake_content:
+  .ascii "cmake_minimum_required(VERSION 3.10)\n\n"
+  .ascii "project(teste)\n\n"
+  .ascii "set(CMAKE_CXX_STANDARD 20)\n"
+  .ascii "set(CMAKE_CXX_STANDARD_REQUIRED True)\n"
+  .ascii "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror\")\n\n"
+  .ascii "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})\n\n"
+  .ascii "add_subdirectory(src)"
+cmake_content_len = . - cmake_content
+main_content:
+  .ascii "#include <iostream>\n\n"
+  .ascii "int main() {\n"
+  .ascii "  std::cout << \"Hello World!\\n\";\n"
+  .ascii "  return 0;\n"
+  .ascii "}"
+main_content_len = . - main_content
+cmake_main_content:
+  .ascii "add_executable(\n"
+  .ascii "  teste\n"
+  .ascii "  main.cpp\n"
+  .ascii ")"
+cmake_main_content_len = . - cmake_main_content
+
+.equ MODE, 0644
+.equ O_CREAT, 0100
+.equ O_WRONLY, 0001
+.equ O_TRUNC, 01000
 
 .section .bss
 cwd: .space 256
+fd: .int 0
 
 .section .text
 .globl _start
@@ -76,27 +104,63 @@ _start:
   movq $proj_main_file_dir, %rdi
   call append_string
 
-.make_files:
-  movq $85, %rax
+.write_files:
+  movq $2, %rax
   movq $proj_cmake_dir, %rdi
-  movq $0222, %rsi
+  movq $O_CREAT | O_WRONLY | O_TRUNC, %rsi
+  movq $MODE, %rdx
+  syscall
+  movq %rax, fd
+
+  movq $1, %rax
+  movq fd, %rdi
+  movq $cmake_content, %rsi
+  movq $cmake_content_len, %rdx
   syscall
 
-  movq $85, %rax
-  movq $proj_cmake_src_dir, %rdi
-  movq $0222, %rsi
+  movq $3, %rax
+  movq $fd, %rdi
   syscall
 
-  movq $85, %rax
+  movq $2, %rax
   movq $proj_main_file_dir, %rdi
-  movq $0222, %rsi
+  movq $O_CREAT | O_WRONLY | O_TRUNC, %rsi
+  movq $MODE, %rdx
+  syscall
+  movq %rax, fd
+
+  movq $1, %rax
+  movq fd, %rdi
+  movq $main_content, %rsi
+  movq $main_content_len, %rdx
+  syscall
+
+  movq $3, %rax
+  movq fd, %rdi
+  syscall
+
+  movq $2, %rax
+  movq $proj_cmake_src_dir, %rdi
+  movq $O_CREAT | O_WRONLY | O_TRUNC, %rsi
+  movq $MODE, %rdx
+  syscall
+  movq %rax, fd
+
+  movq $1, %rax
+  movq fd, %rdi
+  movq $cmake_main_content, %rsi
+  movq $cmake_main_content_len, %rdx
+  syscall
+
+  movq $3, %rax
+  movq fd, %rdi
   syscall
 
 .print_strings:
   movq $1, %rax
   movq $1, %rdi
-  movq $proj_src_dir, %rsi
-  movq $256, %rdx
+  movq $cmake_content, %rsi
+  movq $cmake_content_len, %rdx
   syscall
 
 .exit:
